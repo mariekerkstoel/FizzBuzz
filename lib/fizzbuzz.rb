dcd7c1db9b24fd77589c9b3254ff3e2e0042d12a
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-18 12:23:37 +0100

    wrote fizzbuzz function

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,14 @@
-def fizzbuzz(number)
-	'fizz'
+#!/usr/bin/env ruby
+
+# returns fizz when +n+ is divisible by 3, buzz when divisible by
+# 5, or both when both are true
+def fizzbuzz(n)
+	out = ""
+	if n % 3 == 0
+		out += "fizz"
+	end
+	if n % 5 == 0
+		out += "buzz"
+	end
+	out
 end
