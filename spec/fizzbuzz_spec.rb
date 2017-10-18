6a045ac592e32c625d61f8ce197ef1f42d7d9429
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-18 12:13:07 +0100

    Four specifications for fizzbuzz funtion

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,36 @@
 #!/usr/bin/env ruby
 require "fizzbuzz"
+
 describe 'fizzbuzz' do
-	it 'returns "fizz" when passed 3' do
-		expect(fizzbuzz(3)).to eq 'fizz'
+
+	(0..50).select { |i| (i % 3 == 0) && (i % 5 != 0) }.each do |i|
+	  it "returns 'fizz' when passed #{i}" do
+			expect(fizzbuzz(i)).to eq "fizz"
+	  end
 	end
+
+	(0..50).select { |i| (i % 5 == 0) && (i % 3 != 0) }.each do |i|
+		it "returns 'buzz' when passed #{i}" do
+			expect(fizzbuzz(i)).to eq "buzz"
+		end
+	end
+
+	(0..50).select { |i| (i % 5 == 0) && (i % 3 == 0) }.each do |i|
+		it "returns 'fizzbuzz' when passed #{i}" do
+			expect(fizzbuzz(i)).to eq "fizzbuzz"
+		end
+	end
+
+	(0..50).select { |i| (i % 5 != 0) && (i % 3 != 0) }.each do |i|
+		it "returns '' when passed #{i}" do
+			expect(fizzbuzz(i)).to eq ""
+		end
+	end
+
+
+
+
+
+
+
 end
