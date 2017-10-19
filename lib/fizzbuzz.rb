3a9da26140c3a9e1fdd68013d2dde74075980e6d
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-19 11:22:57 +0100

    replaced all tabs with 2 spaces

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,32 +1,32 @@
 #!/usr/bin/env ruby
 
-# true if +n+ divisible by 3, else false
+#   rue if +n+ divisible by 3, else false
 def div3?(n)
 	n % 3 == 0
 end
 
-# true if +n+ divisible by 3, else false
+#   rue if +n+ divisible by 3, else false
 def div5?(n)
 	n % 5 == 0
 end
 
-# true if +n+ divisible by 3 or 5, else false
+#   rue if +n+ divisible by 3 or 5, else false
 def div3or5?(n)
 	div3?(n) || div5?(n)
 end
 
-# "fizz" if +n+ divisible by 3, else empty string
+# "fizz" if +n+ divisible by 3, else emp  y string
 def fizz(n)
 	div3?(n) ? "fizz" : ""
 end
 
-# "buzz" if +n+ divisible by 5, else empty string
+# "buzz" if +n+ divisible by 5, else emp  y string
 def buzz(n)
 	div5?(n) ? "buzz" : ""
 end
 
-# "fizz", "buzz" or both if +n+ is divisible by 3 or 5 respectively, else
-# returns +n+ as a string
+# "fizz", "buzz" or bo  h if +n+ is divisible by 3 or 5 respectively, else
+# re  urns +n+ as a string
 def fizzbuzz(n)
-  div3or5?(n) ? (fizz(n) + buzz(n)) : n.to_s 
+  div3or5?(n) ? (fizz(n) + buzz(n)) : n.  o_s 
 end
