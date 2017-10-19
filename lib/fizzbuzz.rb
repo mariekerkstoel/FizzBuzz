32ba8c008cd72b98cf315153df962e3cdd88303f
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-19 11:17:01 +0100

    refactored fizzbuzz into smaller components

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,14 +1,32 @@
 #!/usr/bin/env ruby
 
-# returns fizz when +n+ is divisible by 3, buzz when divisible by
-# 5, or both when both are true
+# true if +n+ divisible by 3, else false
+def div3?(n)
+	n % 3 == 0
+end
+
+# true if +n+ divisible by 3, else false
+def div5?(n)
+	n % 5 == 0
+end
+
+# true if +n+ divisible by 3 or 5, else false
+def div3or5?(n)
+	div3?(n) || div5?(n)
+end
+
+# "fizz" if +n+ divisible by 3, else empty string
+def fizz(n)
+	div3?(n) ? "fizz" : ""
+end
+
+# "buzz" if +n+ divisible by 5, else empty string
+def buzz(n)
+	div5?(n) ? "buzz" : ""
+end
+
+# "fizz", "buzz" or both if +n+ is divisible by 3 or 5 respectively, else
+# returns +n+ as a string
 def fizzbuzz(n)
-	out = ""
-	if n % 3 == 0
-		out += "fizz"
-	end
-	if n % 5 == 0
-		out += "buzz"
-	end
-	out
+  div3or5?(n) ? (fizz(n) + buzz(n)) : n.to_s 
 end
