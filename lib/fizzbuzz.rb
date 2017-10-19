cd257d672a7efe92c84d9b135731b6957a2c45ed
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-19 11:41:21 +0100

    actually replaced tabs with 2 spaces

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,12 +3,12 @@
 # returns fizz when +n+ is divisible by 3, buzz when divisible by
 # 5, or both when both are true
 def fizzbuzz(n)
-	out = ""
-	if n % 3 == 0
-		out += "fizz"
-	end
-	if n % 5 == 0
-		out += "buzz"
-	end
-	out
+  out = ""
+  if n % 3 == 0
+    out += "fizz"
+  end
+  if n % 5 == 0
+    out += "buzz"
+  end
+  out
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,29 +3,29 @@ require "fizzbuzz"
 
 describe 'fizzbuzz' do
 
-	(0..50).select { |i| (i % 3 == 0) && (i % 5 != 0) }.each do |i|
-	  it "returns 'fizz' when passed #{i}" do
-			expect(fizzbuzz(i)).to eq "fizz"
-	  end
-	end
+  (0..50).select { |i| (i % 3 == 0) && (i % 5 != 0) }.each do |i|
+    it "returns 'fizz' when passed #{i}" do
+      expect(fizzbuzz(i)).to eq "fizz"
+    end
+  end
 
-	(0..50).select { |i| (i % 5 == 0) && (i % 3 != 0) }.each do |i|
-		it "returns 'buzz' when passed #{i}" do
-			expect(fizzbuzz(i)).to eq "buzz"
-		end
-	end
+  (0..50).select { |i| (i % 5 == 0) && (i % 3 != 0) }.each do |i|
+    it "returns 'buzz' when passed #{i}" do
+      expect(fizzbuzz(i)).to eq "buzz"
+    end
+  end
 
-	(0..50).select { |i| (i % 5 == 0) && (i % 3 == 0) }.each do |i|
-		it "returns 'fizzbuzz' when passed #{i}" do
-			expect(fizzbuzz(i)).to eq "fizzbuzz"
-		end
-	end
+  (0..50).select { |i| (i % 5 == 0) && (i % 3 == 0) }.each do |i|
+    it "returns 'fizzbuzz' when passed #{i}" do
+      expect(fizzbuzz(i)).to eq "fizzbuzz"
+    end
+  end
 
-	(0..50).select { |i| (i % 5 != 0) && (i % 3 != 0) }.each do |i|
-		it "returns '' when passed #{i}" do
-			expect(fizzbuzz(i)).to eq ""
-		end
-	end
+  (0..50).select { |i| (i % 5 != 0) && (i % 3 != 0) }.each do |i|
+    it "returns '' when passed #{i}" do
+      expect(fizzbuzz(i)).to eq ""
+    end
+  end
 
 
 
